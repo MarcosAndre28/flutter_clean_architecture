fd11f63b3eec49a58f155be899f0d8d240733966
MarcosAndre28 <marcosandre1728@gmail.com>
2023-07-03 15:42:46 -0300

    Finalizado

--- lib/config/core/constants/constants.dart
+++ lib/config/core/constants/constants.dart
@@ -1,5 +1,5 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2/';
-const String newsAPIKey = '2ce5f928e4954dd2adc6e3c0234bc733';
+const String newsAPIKey = 'SUA API KEY DO NEWSAPI';
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
 const String kDefaultImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLff5M2CCoLu58Ybuz4BjyfYqTe3Ffv6Mng&usqp=CAU";
